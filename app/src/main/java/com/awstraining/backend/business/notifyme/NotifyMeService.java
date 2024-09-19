d4ad5baf8b8e6fd3a45454d4268a2d4ce3117207
Kamil Zielinski <kamil.zielinski@partner.bmwgroup.com>
2024-09-19 14:54:19 +0200

    [KZ] Pushed some additional changes lab3

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -15,11 +15,13 @@ public class NotifyMeService {
     
     private final MessageSender messageSender;
     private final Translator translator;
+    private final Sentiment sentiment;
     
     @Autowired
-    public NotifyMeService(MessageSender messageSender, Translator translator) {
+    public NotifyMeService(MessageSender messageSender, Translator translator, Sentiment sentiment) {
         this.messageSender = messageSender;
         this.translator = translator;
+        this.sentiment = sentiment;
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
@@ -31,11 +33,14 @@ public class NotifyMeService {
         //  1. Translate text from using translator.
         String translatedText = translator.translate(notifyMe);
         //  2. Change sending of text to "translated text" and return it.
-        messageSender.send(translatedText);
         // TODO: lab3
         //  1. Detect sentiment of translated message.
+        String sentimentValue = sentiment.detectSentiment(translatedText, notifyMe.targetLc());
+
+        String resultText = "%s: %s".formatted(sentimentValue, translatedText);
+        messageSender.send(resultText);
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return translatedText;
+        return resultText;
     }
     
 }
--- app/src/main/java/com/awstraining/backend/business/notifyme/adapter/SentimentDetectorImpl.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/adapter/SentimentDetectorImpl.java
@@ -1,8 +1,12 @@
 package com.awstraining.backend.business.notifyme.adapter;
 
+import com.amazonaws.services.comprehend.AmazonComprehend;
+import com.amazonaws.services.comprehend.model.DetectSentimentRequest;
+import com.amazonaws.services.comprehend.model.DetectSentimentResult;
 import com.awstraining.backend.business.notifyme.Sentiment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,18 +18,26 @@ public class SentimentDetectorImpl implements Sentiment {
     
     // TODO: lab3
     //  1. Inject AWS AmazonComprehend from configuration ComprehendSentimentConfig.
-//    @Autowired
-    public SentimentDetectorImpl() {
-        
+
+    private final AmazonComprehend amazonComprehend;
+
+    @Autowired
+    public SentimentDetectorImpl(AmazonComprehend amazonComprehend) {
+        this.amazonComprehend = amazonComprehend;
     }
 
     // TODO: lab3
-    //  1. Create detect sentiment text request.
-    //  2. Call detection.
-    //  3. Log information about sentiment.
-    //  4. Return sentiment.
     @Override
     public String detectSentiment(String language, String text) {
-      return "";
+    //  1. Create detect sentiment text request.
+        DetectSentimentRequest detectSentimentRequest = new DetectSentimentRequest();
+        detectSentimentRequest.setText(text);
+        detectSentimentRequest.setLanguageCode(language);
+    //  2. Call detection
+        DetectSentimentResult detectSentimentResult = amazonComprehend.detectSentiment(detectSentimentRequest);
+    //  3. Log information about sentiment.
+        LOGGER.info("Detected sentiment: {}", detectSentimentResult.getSentiment());
+    //  4. Return sentiment.
+        return detectSentimentResult.getSentiment();
     }
 }
